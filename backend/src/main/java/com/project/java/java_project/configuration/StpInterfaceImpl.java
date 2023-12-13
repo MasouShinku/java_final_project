7e1cdac4a6c24bf57f94862addd45261120240c6
MasouShinku <2389837103@qq.com>
2023-12-14 00:28:00 +0800

    增加权限校验接口

---
+++ backend/src/main/java/com/project/java/java_project/configuration/StpInterfaceImpl.java
@@ -0,0 +1,37 @@
+package com.project.java.java_project.configuration;
+
+import cn.dev33.satoken.stp.StpInterface;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自定义权限加载接口实现类
+ */
+@Component    // 保证此类被 SpringBoot 扫描，完成 Sa-Token 的自定义权限验证扩展
+public class StpInterfaceImpl implements StpInterface {
+
+    /**
+     * 返回一个账号所拥有的权限码集合
+     */
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+//        由于系统比较简单，直接添加全部user权限就好
+        List<String> list = new ArrayList<String>();
+        list.add("user.*");
+        return list;
+    }
+
+    /**
+     * 返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
+     */
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+//        同理，直接声明用户角色
+        List<String> list = new ArrayList<String>();
+        list.add("user");
+        return list;
+    }
+
+}
